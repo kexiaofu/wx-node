426b6232ecbe4c49512ada970613b2d9fc88f8bb
liangjiafu <liangjiafu@chengyisheng.com.cn>
2018-05-30 01:12:58 +0800

    已经实现了身份登录记录在数据库了

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
 let express = require('express'),
     app = express(),
+    bodyParser = require('body-parser'),
     axios = require('axios'),
     redis = require('redis'),
+    query = require('./mysql');
     client = redis.createClient();
+
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(bodyParser.json());
 
 let appid = 'wx0f3091f747b37832',
     appsecret = 'd85dd12090f55ee18eb1e123d647edd7';
@@ -15,29 +20,95 @@ client.hgetall('session',(err,obj)=>{
   console.log(obj);
 });
 
-setTimeout(()=>{
-  client.hgetall('session',(err,obj)=>{
-    console.log(obj);
-  });
-},3000);
+let querySomeOne = (openid,cb) =>{
+  console.log(openid);
+  query('select nickName,avatarUrl,gender,city,province,country,language from users where openid = "'+openid+'" order by id desc',(qerr,vals,fields)=>{
+    if(qerr) {
+      console.log(qerr);
+    }
+    console.log(vals);
 
-app.get('/api/getcode',(req,res)=>{
+    typeof cb === 'function' && cb(vals);
+  })
+};
+
+app.get('/api/queryUser',(req,res)=>{
+  let openid = req.query.id;
+
+  console.log(openid,req.query);
+
+  querySomeOne(openid,vals=>{
+    console.log(vals,'------queryUser-----')
+    if(vals.length !== 0) {
+      res.send({
+        code:2000,
+        data:vals
+      })
+    } else {
+      res.send({
+        code:4004,
+        data:[]
+      })
+    }
+  });
+
+
+
+});
+
+app.post('/api/setUserInfo',(req,res)=>{
+  console.log(req);
+
+  let obj = req.body;
+
+  query('set names utf8',(qrr,vals,fields)=>{});
+
+  query(`insert into users (openid,nickName,avatarUrl,gender,city,province,country,language) values ('${obj.openid}','${obj.nickName}','${obj.avatarUrl}','${obj.gender}','${obj.city}','${obj.province}','${obj.country}','${obj.language}')`,(qerr,vals,fields)=>{
+    if(qerr) {
+      console.log(qerr);
+    }
+    console.log(vals);
+  });
+
+  res.send({
+    code:2000,
+    data:[]
+  });
+
+
+});
+
+app.get('/api/getUserInfo',(req,res)=>{
     let code = req.query.code;
 
     console.log(code);
 
 
     axios.get(`https://api.weixin.qq.com/sns/jscode2session?appid=${appid}&secret=${appsecret}&js_code=${code}&grant_type=authorization_code`)
-      .then(res=>{
-          if(res.data) {
-            console.log(res.data);
-            let data = res.data;
-            res.send({
-              code:2000,
-              data:{
-                clientId:data.openid
+      .then(s=>{
+          if(s.data) {
+            console.log(s.data);
+            let data = s.data,
+                hadRecord = false,
+                recordData={};
+
+            querySomeOne(data.openid,vals=>{
+              if(vals.length !== 0) {
+                hadRecord = true;
+                recordData = vals;
               }
-            })
+
+              res.send({
+                code:2000,
+                data:{
+                  clientId:data.openid,
+                  hadRecord:hadRecord,
+                  record:recordData
+                }
+              })
+            });
+
+
           }
       })
       .catch(error=>{
---
+++ src/mysql.js
@@ -0,0 +1,26 @@
+var mysql      = require('mysql');
+var pool = mysql.createPool({
+  host     : '120.79.72.55',
+  user     : 'root',
+  password : '817822ke',
+  database : 'wx_mp_doctor',
+  insecureAuth:true,
+  charset:'utf8'
+});
+
+var query=function(sql,callback){
+    pool.getConnection(function(err,conn){
+        if(err){
+            callback(err,null,null);
+        }else{
+            conn.query(sql,function(qerr,vals,fields){
+                //释放连接
+                conn.release();
+                //事件驱动回调
+                callback(qerr,vals,fields);
+            });
+        }
+    });
+};
+
+module.exports=query;
